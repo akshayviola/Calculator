fe69307a8ab5bb0f78e9df7aac45fd88dae82096
akshayviola <akshaysunil201@gmail.com>
2023-08-07 19:23:47 +0530

    Calculator

---
+++ main.js
@@ -0,0 +1,18 @@
+function display(num){
+    // id.value=to get value
+    output.value += num;  
+}
+
+function clearAll(){
+    output.value=""
+}
+// evaluate
+
+function evaluateExp(){
+    output.value=eval(output.value)
+}
+
+function removeLast(){
+    currentExp=output.value;
+    output.value=currentExp.slice(0,-1)
+}
